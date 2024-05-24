63551c55b4b9b8332eece5988cfb2ec52a0e8ebe
Bujji Setty <ranganath1000@gmail.com>
2024-05-24 13:45:44 -0500

    Suppressing output to reduce log file bloat

--- download.sh
+++ download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #wget https://suitesparse-collection-website.herokuapp.com/MM/MAWI/mawi_201512020330.tar.gz
-wget https://suitesparse-collection-website.herokuapp.com/MM/HB/arc130.tar.gz
+wget https://suitesparse-collection-website.herokuapp.com/MM/HB/arc130.tar.gz -q
